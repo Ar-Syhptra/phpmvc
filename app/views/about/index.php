904aed89a2fc7c6c51378a0a2f8516128c962b05
Ar-Syhptra <theark338@gmail.com>
2024-12-15 11:11:15 +0700

    phpmvc-6

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,9 @@
-
-  <h1>About Me</h1>
-  <p>Halo, Nama Saya <?= $data['nama']; ?>, Umur Saya <?= $data['umur']; ?> Tahun, Saya Adalah Seorang <?= $data['pekerjaan']; ?></p>
+<div class="container">
+  <h1 class="mt-4">Ab
+  out Me</h1>
+  <img src="<?=  BASEURL; ?>/img/profile.jpg" alt="Arkansyahputra" width="200"
+  class="rounded-circle shadow"/>
+  <p>Halo, Nama Saya <?= $data["nama"] ?>, Umur Saya <?= $data[
+ "umur"
+] ?> Tahun, Saya Adalah Seorang <?= $data["pekerjaan"] ?></p>
+</div>
